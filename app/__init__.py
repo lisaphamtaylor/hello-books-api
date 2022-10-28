7ac790db840ec15e4be1857718a8f1daddb844f5
Lisa <elizabethpham726@gmail.com>
2022-10-27 17:14:45 -0700

    adds Book model with SQLAlchemy and Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
